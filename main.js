8403c1828d03ed6aeff2250d4eba7bfdf1d5e380
Manuell00 <manuell.caselli@gmail.com>
2023-05-02 15:22:07 +0200

    Imposto la pagina

---
+++ main.js
@@ -0,0 +1,3 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
